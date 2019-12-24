b89db0596719688a1f27cb3956bcc07704c0f58c
Mykhailo Kramar <3805033@gmail.com>
2019-12-24 17:07:27 +0200

    hw11

--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -1,10 +1,17 @@
 package core.basesyntax;
 
+import java.util.function.Supplier;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для случайного
  * выбора цвета</p>
  */
-public class RandomColor {
+public class RandomColor implements Supplier<String> {
     private static final String[] COLORS = {"red", "green", "blue", "yellow", "pink",
             "black", "white"};
+
+    public String get() {
+        Supplier<String> supplier = () -> COLORS[(int) (Math.random() * COLORS.length)];
+        return supplier.get();
+    }
 }
