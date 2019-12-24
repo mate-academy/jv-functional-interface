d6be7f64e06e7bc177e9e3f86759e10080705c06
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-24 16:16:19 +0200

    added changes

--- src/main/java/core/basesyntax/CheckElectricCar.java
+++ src/main/java/core/basesyntax/CheckElectricCar.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
+import java.util.function.Predicate;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод, который реализует проверку
  * установлен ли на авто EngineType.ELECTRIC. Метод должен принимать Car.</p>
  */
-public class CheckElectricCar implements CheckEngine<Car> {
+public class CheckElectricCar implements Predicate<Car> {
 
     @Override
     public boolean test(Car car) {
-        CheckEngine<Car> checkEngine =
+        Predicate<Car> checkEngine =
                 currentCar -> currentCar.getEngine().getEngineType()
                         .equals(EngineType.ELECTRIC);
         return checkEngine.test(car);
--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -1,18 +1,19 @@
 package core.basesyntax;
 
 import java.util.Random;
+import java.util.function.Supplier;
 
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для случайного
  * выбора цвета</p>
  */
-public class RandomColor implements PickColor {
+public class RandomColor implements Supplier<String> {
     private static final String[] COLORS = {"red", "green", "blue", "yellow", "pink",
             "black", "white"};
 
     @Override
     public String get() {
-        PickColor pickColor = () -> COLORS[new Random().nextInt(COLORS.length)];
+        Supplier<String> pickColor = () -> COLORS[new Random().nextInt(COLORS.length)];
         return pickColor.get();
     }
 }
