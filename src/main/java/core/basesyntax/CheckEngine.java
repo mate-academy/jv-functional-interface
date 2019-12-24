d38aa99d8820a44bf876337af03b9be5d2b4e291
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-24 15:04:15 +0200

    changed solution

--- src/main/java/core/basesyntax/CheckEngine.java
+++
@@ -1,6 +0,0 @@
-package core.basesyntax;
-
-@FunctionalInterface
-public interface CheckEngine<T> {
-    boolean test(T car);
-}
--- src/main/java/core/basesyntax/Comparator.java
+++
@@ -1,6 +0,0 @@
-package core.basesyntax;
-
-@FunctionalInterface
-public interface Comparator {
-    Integer apply(Integer firstVolume, Integer secondVolume);
-}
