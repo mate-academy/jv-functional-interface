ec31d3743c4945724e49475d5ced474ae13cbdcd
Alexandr Sashchenko <santys87@gmail.com>
2019-12-25 22:39:07 +0200

    HW 11 Solution added

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,9 +1,17 @@
 package core.basesyntax;
 
+import java.util.function.BiFunction;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
  * двигателей у двух авто (enginePower) и возврата разницы между ними.
  * Метод должен принимать Integer.</p>
  */
-public class CompareTwoAuto {
+public class CompareTwoAuto implements BiFunction<Integer, Integer, Integer> {
+
+    @Override
+    public Integer apply(Integer carEngine1, Integer carEngine2) {
+        BiFunction<Integer, Integer, Integer> compare = (engine1, engine2) -> (engine1 - engine2);
+        return compare.apply(carEngine1, carEngine2);
+    }
 }
