b42de9da8d4e99987baf15027cf13e6831d0932c
Nick97-git <nickarchangel97@gmail.com>
2020-04-15 14:53:30 +0300

    added constant to ChangeToCar class

--- src/main/java/core/basesyntax/ChangeToCar.java
+++ src/main/java/core/basesyntax/ChangeToCar.java
@@ -8,8 +8,10 @@ import java.util.function.Function;
  * TRUCK). Метод должен принимать Integer и возвращать AutoType</p>
  */
 public class ChangeToCar implements Function<Integer,AutoType> {
+    private static final int MAX_WEIGHT = 1500;
+    
     @Override
     public AutoType apply(Integer weight) {
-        return weight < 1500 ? AutoType.CAR : AutoType.TRUCK;
+        return weight < MAX_WEIGHT ? AutoType.CAR : AutoType.TRUCK;
     }
 }
