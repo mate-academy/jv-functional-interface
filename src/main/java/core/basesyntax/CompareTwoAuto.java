b4682f94779259dbf1c48f523582482480f3de52
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-15 15:26:45 +0300

    added solution

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -5,5 +5,9 @@ package core.basesyntax;
  * двигателей у двух авто (enginePower) и возврата разницы между ними.
  * Метод должен принимать Integer.</p>
  */
-public class CompareTwoAuto {
+public class CompareTwoAuto implements ComparePower {
+    @Override
+    public int apply(Integer engineVolume1, Integer engineVolume2) {
+        return engineVolume1 - engineVolume2;
+    }
 }
