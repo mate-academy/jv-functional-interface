3cfb66fcac0d522ba700bec5c7961df485b6618c
VBortnyk <viacheslav.bortnyk@gmail.com>
2020-04-15 21:56:17 +0300

    changed solution according to the task

--- src/main/java/core/basesyntax/ChooseType.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax;
-
-@FunctionalInterface
-public interface ChooseType {
-    public AutoType apply(Integer weight);
-
-}
--- src/main/java/core/basesyntax/ComparePower.java
+++
@@ -1,6 +0,0 @@
-package core.basesyntax;
-
-@FunctionalInterface
-public interface ComparePower {
-    public Integer apply(Integer engineVolume, Integer engineVolume1);
-}
