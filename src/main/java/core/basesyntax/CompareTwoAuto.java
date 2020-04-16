7b6176d8a195005212334b4e8aef85b26dbd5ded
petrukillya <petrukillya98@gmail.com>
2020-04-16 16:13:33 +0300

    added solution

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,9 +1,14 @@
 package core.basesyntax;
 
+import java.util.function.BinaryOperator;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
  * двигателей у двух авто (enginePower) и возврата разницы между ними.
  * Метод должен принимать Integer.</p>
  */
-public class CompareTwoAuto {
+public class CompareTwoAuto implements BinaryOperator<Integer> {
+    public Integer apply(Integer engineVolume, Integer engineVolume1) {
+        return engineVolume - engineVolume1;
+    }
 }
