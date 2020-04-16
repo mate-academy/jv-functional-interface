b3aff31a642234531d53f42aefbab93b88eedced
Allduiin <vovapro01@gmail.com>
2020-04-16 13:20:08 +0300

    Added solution

--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -10,13 +10,8 @@ import java.util.function.Supplier;
 public class RandomColor implements Supplier<String> {
     private static final String[] COLORS = {"red", "green", "blue", "yellow", "pink",
             "black", "white"};
-    private String color;
-
-    public RandomColor() {
-        color = COLORS[new Random().nextInt(COLORS.length)];
-    }
 
     public String get() {
-        return color;
+        return COLORS[new Random().nextInt(COLORS.length)];
     }
 }
