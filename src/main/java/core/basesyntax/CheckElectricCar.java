792252c1b38338f98712093e3069f1b8c80788e0
olehedza <olehedza@gmail.com>
2020-04-16 15:09:22 +0300

    implement 4 functional interfaces

--- src/main/java/core/basesyntax/CheckElectricCar.java
+++ src/main/java/core/basesyntax/CheckElectricCar.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
+import java.util.function.Predicate;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод, который реализует проверку
  * установлен ли на авто EngineType.ELECTRIC. Метод должен принимать Car.</p>
  */
-public class CheckElectricCar {
+public class CheckElectricCar implements Predicate<Car> {
+    public static final EngineType ELECTRIC = EngineType.ELECTRIC;
+
+    @Override
+    public boolean test(Car t) {
+        return t.getEngine().getEngineType() == ELECTRIC;
+    }
 }
--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,9 +1,16 @@
 package core.basesyntax;
 
+import java.util.function.BinaryOperator;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
  * двигателей у двух авто (enginePower) и возврата разницы между ними.
  * Метод должен принимать Integer.</p>
  */
-public class CompareTwoAuto {
+public class CompareTwoAuto implements BinaryOperator<Integer> {
+
+    @Override
+    public Integer apply(Integer integer, Integer integer2) {
+        return integer - integer2;
+    }
 }
