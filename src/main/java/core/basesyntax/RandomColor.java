9cff35401a08f103f62f5ea274cc2d6be9b7b61b
nastya <cctvporsh@gmail.com>
2020-04-16 08:05:24 +0300

    Added solution to the Functinal-interface task

--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -1,10 +1,19 @@
 package core.basesyntax;
 
+import java.util.Random;
+import java.util.function.Supplier;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для случайного
  * выбора цвета</p>
  */
-public class RandomColor {
+public class RandomColor implements Supplier<String> {
     private static final String[] COLORS = {"red", "green", "blue", "yellow", "pink",
             "black", "white"};
+
+    @Override
+    public String get() {
+        int rand = new Random().nextInt(COLORS.length);
+        return COLORS[rand];
+    }
 }
