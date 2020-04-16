24f500682b0370c00caf9b33131287fa8c01ecc6
Володимир <matrix.vova@gmail.com>
2020-04-16 18:31:42 +0300

    added solution

--- src/main/java/core/basesyntax/CheckElectricCar.java
+++ src/main/java/core/basesyntax/CheckElectricCar.java
@@ -1,8 +1,19 @@
 package core.basesyntax;
 
+import java.util.function.Predicate;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод, который реализует проверку
  * установлен ли на авто EngineType.ELECTRIC. Метод должен принимать Car.</p>
  */
-public class CheckElectricCar {
+public class CheckElectricCar implements Predicate<Car> {
+
+    @Override
+    public boolean test(Car car) {
+
+        if (car.getEngine().getEngineType() == EngineType.ELECTRIC) {
+            return true;
+        }
+        return false;
+    }
 }
