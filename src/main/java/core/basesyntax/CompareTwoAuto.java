3adcf4f4ce852c1242dab9eae97c1148b1e2bba9
ihor <krohmal.igor777@gmail.com>
2020-04-16 12:22:40 +0300

    changed functional interface in class CompareTwoAuto on BinaryOperator

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
-import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
  * двигателей у двух авто (enginePower) и возврата разницы между ними.
  * Метод должен принимать Integer.</p>
  */
-public class CompareTwoAuto implements BiFunction<Integer,Integer,Integer> {
+public class CompareTwoAuto implements BinaryOperator<Integer> {
 
     @Override
     public Integer apply(Integer engineVolume1, Integer engineVolume2) {
