247167ae30d1c03827cce50db6b117f203006e42
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-16 15:13:47 +0300

    No Constants

--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -8,12 +8,11 @@ import java.util.function.Supplier;
  * выбора цвета</p>
  */
 public class RandomColor implements Supplier<String> {
-    private static final int COLOR_INDEX_BOUND = 6;
     private static final String[] COLORS = {"red", "green", "blue", "yellow", "pink",
             "black", "white"};
 
     @Override
     public String get() {
-        return COLORS[new Random().nextInt(COLOR_INDEX_BOUND)];
+        return COLORS[new Random().nextInt(COLORS.length)];
     }
 }
