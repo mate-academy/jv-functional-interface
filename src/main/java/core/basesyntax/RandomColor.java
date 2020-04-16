3198c4ff9571750d4e94b7ecc62e1cb5190544f8
Maryna <kaijou88@gmail.com>
2020-04-16 16:16:41 +0300

    added solution

--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -1,10 +1,19 @@
 package core.basesyntax;
 
+import java.util.Random;
+import java.util.function.Supplier;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для случайного
  * выбора цвета</p>
  */
-public class RandomColor {
+public class RandomColor implements Supplier {
     private static final String[] COLORS = {"red", "green", "blue", "yellow", "pink",
             "black", "white"};
+
+    @Override
+    public String get() {
+        Random random = new Random();
+        return COLORS[random.nextInt(COLORS.length)];
+    }
 }
