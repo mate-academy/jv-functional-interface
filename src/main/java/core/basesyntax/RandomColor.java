5c6cc847f7463644a159a473c6954e311bf17b34
Andrew <tolstuy1996@gmail.com>
2020-04-17 12:39:08 +0300

    fixed

--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 /**
@@ -12,6 +13,6 @@ public class RandomColor implements Supplier<String> {
 
     @Override
     public String get() {
-        return COLORS[(int) (Math.random() * 5 + 1)];
+        return COLORS[new Random().nextInt(COLORS.length)];
     }
 }
