282cf2f65199ea9b02e6b7500b54275fd6360c82
Kostian <pyteec.ua@i.ua>
2020-08-19 22:06:11 +0300

    added solution

--- src/main/java/core/basesyntax/CheckElectricCar.java
+++ src/main/java/core/basesyntax/CheckElectricCar.java
@@ -1,8 +1,14 @@
 package core.basesyntax;
 
+import java.util.function.Predicate;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод, который реализует проверку
  * установлен ли на авто EngineType.ELECTRIC. Метод должен принимать Car.</p>
  */
-public class CheckElectricCar {
+public class CheckElectricCar implements Predicate<Car> {
+    @Override
+    public boolean test(Car car) {
+        return car.getEngine().getEngineType().equals(EngineType.ELECTRIC);
+    }
 }
