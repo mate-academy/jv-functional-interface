ccec563d137b4f82ceff2c4a3fd5d23e6418ee13
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-08-19 14:05:57 +0300

    implemented BiFunction in CompareTwoAuto

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,9 +1,15 @@
 package core.basesyntax;
 
+import java.util.function.BiFunction;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
  * двигателей у двух авто (enginePower) и возврата разницы между ними.
  * Метод должен принимать Integer.</p>
  */
-public class CompareTwoAuto {
+public class CompareTwoAuto implements BiFunction<Car, Car, Integer> {
+    @Override
+    public Integer apply(Car firstCar, Car secondCar) {
+        return Math.abs(firstCar.getEngine().getEngineVolume() - secondCar.getEngine().getEngineVolume());
+    }
 }
