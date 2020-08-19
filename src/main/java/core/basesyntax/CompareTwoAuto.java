6de189b63b3696ba2aff7dbf831417e91940ac4f
romanHreits <romagreic@gmail.com>
2020-08-19 15:21:58 +0300

    deleted comments

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -2,11 +2,6 @@ package core.basesyntax;
 
 import java.util.function.BinaryOperator;
 
-/**
- * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
- * двигателей у двух авто (enginePower) и возврата разницы между ними.
- * Метод должен принимать Integer.</p>
- */
 public class CompareTwoAuto implements BinaryOperator<Integer> {
 
     @Override
