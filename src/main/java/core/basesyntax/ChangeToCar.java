e91317f4093d132a3a748a5c67bcfd4bd6d1cc51
dariiapikul <dariyapikul@gmail.com>
2020-08-19 17:43:43 +0300

    Checkstyle fixed

--- src/main/java/core/basesyntax/ChangeToCar.java
+++ src/main/java/core/basesyntax/ChangeToCar.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
  * и возвращает тип авто в зависимости от веса (если weight меньше 1500, то CAR, если больше, то
  * TRUCK). Метод должен принимать Integer и возвращать AutoType</p>
  */
+
 public class ChangeToCar implements Function<Integer, AutoType> {
     private static final int REQUIRED_WEIGHT = 1500;
 
--- src/main/java/core/basesyntax/RandomColor.java
+++ src/main/java/core/basesyntax/RandomColor.java
@@ -6,6 +6,7 @@ import java.util.function.Supplier;
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для случайного
  * выбора цвета</p>
  */
+
 public class RandomColor implements Supplier<String> {
     private static final String[] COLORS = {"red", "green", "blue", "yellow", "pink",
             "black", "white"};
