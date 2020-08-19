b28f23408e2fca2ec6096cebe00c3ee0f79a7f11
dariiapikul <dariyapikul@gmail.com>
2020-08-19 20:51:07 +0300

    Changed variable's name

--- src/main/java/core/basesyntax/ChangeToCar.java
+++ src/main/java/core/basesyntax/ChangeToCar.java
@@ -8,10 +8,10 @@ import java.util.function.Function;
  */
 
 public class ChangeToCar implements Function<Integer, AutoType> {
-    private static final int REQUIRED_WEIGHT = 1500;
+    private static final int MAX_CAR_WEIGHT = 1500;
 
     @Override
     public AutoType apply(Integer weight) {
-        return weight > REQUIRED_WEIGHT ? AutoType.TRUCK : AutoType.CAR;
+        return weight > MAX_CAR_WEIGHT ? AutoType.TRUCK : AutoType.CAR;
     }
 }
