0f4af29443b947d5993938a9352e799970ac1b12
Roman <matviichuk.office@gmail.com>
2020-08-20 08:43:36 +0300

    improved naming

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -10,7 +10,7 @@ import java.util.function.BinaryOperator;
 public class CompareTwoAuto implements BinaryOperator<Integer> {
 
     @Override
-    public Integer apply(Integer engineVolume1, Integer engineVolume2) {
-        return engineVolume1 - engineVolume2;
+    public Integer apply(Integer firstPower, Integer secondPower) {
+        return firstPower - secondPower;
     }
 }
