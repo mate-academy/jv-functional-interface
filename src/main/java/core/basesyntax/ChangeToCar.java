a780b3b3f0a66288563c824d32efe59897d5dae0
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-20 11:49:42 +0300

    Namming changes

--- src/main/java/core/basesyntax/ChangeToCar.java
+++ src/main/java/core/basesyntax/ChangeToCar.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.function.Function;
 
 public class ChangeToCar implements Function<Integer, AutoType> {
-    private static final int WEIGHT = 1500;
+    private static final int CRITICAL_CAR_WEIGHT = 1500;
 
     @Override
     public AutoType apply(Integer weight) {
-        return weight > WEIGHT ? AutoType.TRUCK : AutoType.CAR;
+        return weight > CRITICAL_CAR_WEIGHT ? AutoType.TRUCK : AutoType.CAR;
     }
 }
--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -4,7 +4,7 @@ import java.util.function.BinaryOperator;
 
 public class CompareTwoAuto implements BinaryOperator<Integer> {
     @Override
-    public Integer apply(Integer integer, Integer integer2) {
-        return integer - integer2;
+    public Integer apply(Integer firstCarEngineVolume, Integer secondCarEngineVolume) {
+        return firstCarEngineVolume - secondCarEngineVolume;
     }
 }
