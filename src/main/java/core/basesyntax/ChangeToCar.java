504c6ef2bff77bfd9122fe2749929c83b45c45df
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-20 11:30:04 +0300

    Initial commit

--- src/main/java/core/basesyntax/ChangeToCar.java
+++ src/main/java/core/basesyntax/ChangeToCar.java
@@ -1,9 +1,12 @@
 package core.basesyntax;
 
-/**
- * <p>Имплементируйте необходимый функциональный интерфейс и его метод, который принимает вес авто
- * и возвращает тип авто в зависимости от веса (если weight меньше 1500, то CAR, если больше, то
- * TRUCK). Метод должен принимать Integer и возвращать AutoType</p>
- */
-public class ChangeToCar {
+import java.util.function.Function;
+
+public class ChangeToCar implements Function<Integer, AutoType> {
+    private static final int WEIGHT = 1500;
+
+    @Override
+    public AutoType apply(Integer weight) {
+        return weight > WEIGHT ? AutoType.TRUCK : AutoType.CAR;
+    }
 }
--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
-/**
- * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
- * двигателей у двух авто (enginePower) и возврата разницы между ними.
- * Метод должен принимать Integer.</p>
- */
-public class CompareTwoAuto {
+import java.util.function.BinaryOperator;
+
+public class CompareTwoAuto implements BinaryOperator<Integer> {
+    @Override
+    public Integer apply(Integer integer, Integer integer2) {
+        return integer - integer2;
+    }
 }
