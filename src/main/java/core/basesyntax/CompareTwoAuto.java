6229b48b68749a436df5bb8a9c852d2cb00bf76d
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-08-20 14:14:19 +0300

    task done

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,9 +1,16 @@
 package core.basesyntax;
 
+import java.util.function.BinaryOperator;
+
 /**
  * <p>Имплементируйте необходимый функциональный интерфейс и его метод для сравнения мощности
  * двигателей у двух авто (enginePower) и возврата разницы между ними.
  * Метод должен принимать Integer.</p>
  */
-public class CompareTwoAuto {
+public class CompareTwoAuto implements BinaryOperator<Integer> {
+
+    @Override
+    public Integer apply(Integer enginePower1, Integer enginePower2) {
+        return enginePower1 - enginePower2;
+    }
 }
