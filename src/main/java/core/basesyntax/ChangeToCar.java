e034b123ce2ff3415bdc793295b28e5e2f8bdfff
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-08-20 10:41:37 +0300

    constanta added

--- src/main/java/core/basesyntax/ChangeToCar.java
+++ src/main/java/core/basesyntax/ChangeToCar.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 import java.util.function.Function;
 
 public class ChangeToCar implements Function<Integer, AutoType> {
+    public static final Integer MAX_CAR_WEIGHT = 1500;
 
     @Override
     public AutoType apply(Integer weight) {
-        return weight < 1500 ? AutoType.CAR : AutoType.TRUCK;
+        return weight < MAX_CAR_WEIGHT ? AutoType.CAR : AutoType.TRUCK;
     }
 }
