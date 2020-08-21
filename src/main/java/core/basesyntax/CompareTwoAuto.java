49c1ae2f4df08e0325e4484ad323a554d116b070
Jah <moskovienkor@gmail.com>
2020-08-21 15:11:25 +0300

    hw18: - done; - fixed: custom classes/interfaces added;

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -5,7 +5,7 @@ import java.util.function.BinaryOperator;
 public class CompareTwoAuto implements BinaryOperator<Integer> {
 
     @Override
-    public Integer apply(Integer volume1, Integer volume2) {
-        return volume1 - volume2;
+    public Integer apply(Integer engineOneVolume, Integer engineTwoVolume) {
+        return engineOneVolume - engineTwoVolume;
     }
 }
