08134d6591716df127a3b84a74ead50f1e61490a
dmytro-hryhoruk <dimamyhgrygoruk@gmail.com>
2020-08-25 09:32:04 +0300

    removed long lines

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 /**
- * <p> Implement the suitable functional interface and override its method for engines power comparison
+ * <p> Implement the suitable functional interface and override its method
+ * for engines power comparison
  * in two cars (enginePower) and return the difference between them.
  * The method must accept Integer. </p>
  */
