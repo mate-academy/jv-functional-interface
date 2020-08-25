bde4e09cb150da9e01817581806e042a3b884f75
dmytro-hryhoruk <dimamyhgrygoruk@gmail.com>
2020-08-25 09:33:44 +0300

    formatted text

--- src/main/java/core/basesyntax/CompareTwoAuto.java
+++ src/main/java/core/basesyntax/CompareTwoAuto.java
@@ -2,9 +2,8 @@ package core.basesyntax;
 
 /**
  * <p> Implement the suitable functional interface and override its method
- * for engines power comparison
- * in two cars (enginePower) and return the difference between them.
- * The method must accept Integer. </p>
+ * for engines power comparison in two cars (enginePower)
+ * and return the difference between them. The method must accept Integer. </p>
  */
 public class CompareTwoAuto {
 }
