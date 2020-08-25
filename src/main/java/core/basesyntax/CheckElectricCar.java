1baa476172a46d743622fe03956a6dbde14aa4cb
dmytro-hryhoruk <dimamyhgrygoruk@gmail.com>
2020-08-25 10:01:33 +0300

    translated random color class

--- src/main/java/core/basesyntax/CheckElectricCar.java
+++ src/main/java/core/basesyntax/CheckElectricCar.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 /**
  * <p> Implement the suitable functional interface and override its method that makes validation
- * whether EngineType.ELECTRIC installed on auto. The method must accept Car. </p>
+ * whether EngineType.ELECTRIC is installed on an auto. The method must accept Car. </p>
  */
 public class CheckElectricCar {
 }
